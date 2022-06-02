c38abf41e60506dc200da118fa0355c5b60da13b
Matthino868 <74274573+Matthino868@users.noreply.github.com>
2022-06-02 12:01:38 +0200

    withdraw request toegevoegd, error handling toegevoegd

--- Database/apitest.py
+++ Database/apitest.py
@@ -1,14 +1,15 @@
 import requests
 import json
 
-r = requests.get('http://145.24.222.71:8443/test', verify='C:\school\Vakken\Project34-PinAutomaat\Keys\mk-server-chain.pem')
+# r = requests.get('http://145.24.222.82:8443/test', verify='./keys/noob_root_chain.pem')
 # r = requests.get('https://google.com', verify=False)
 # print(dir(r))
 # print(r.text)
 # print(r.content)
 # print(r.raw)
 # print(r.json)
-print(str(r.status_code)+ '\n')
+# print(str(r.status_code)+ '\n')
+
 
 headers = {"Content-Type": "application/json"}
 
@@ -16,34 +17,38 @@ print("=========Withdraw Request=========")
 
 withdrawRequest = {
    "head":{
-      "fromCtry":"T1",
-      "fromBank":"TES",
-      "toCtry":"T1",
-      "toBank":"TES"
+      "fromCtry":"MK",
+      "fromBank":"BANQ",
+      "toCtry":"MK",
+      "toBank":"BANQ"
    },
    "body":{
-      "acctNo":"5",
-      "pin":"0101",
-      "amount": 5000
+      "acctNo":"MKBANQ0000000300",
+      "pin":"0001",
+      "amount": 50
    }
 }
 wr_data = json.dumps(withdrawRequest)
 
 w = requests.post('http://145.24.222.71:8443/withdraw',
-    cert = ('C:\school\Vakken\Project34-PinAutomaat\Keys\mk-server-chain.pem', 'C:\school\Vakken\Project34-PinAutomaat\Keys\country_key.pem'),
-    verify='C:\school\Vakken\Project34-PinAutomaat\Keys\mk-server-chain.pem',
-    headers=headers,
-    data=wr_data
+   cert = ('./keys/mk_server_chain.pem', './keys/country_key.pem'),
+   verify='./keys/noob_root.pem',
+   headers=headers,
+   data=wr_data
 )
 # print(dir(w))
 # print(w.text)
 
-
-data = json.loads(json.loads(w.text))
-print('Withdraw status: ' + str(w.status_code))
-print("Account number: " + data['body']['acctNo'])
-print("Success status: "+ str(data['body']['success']))
-print("Withdraw amount: " +str(withdrawRequest["body"]["amount"]))
+try:
+   data = json.loads(json.loads(w.text))
+except:
+   print(w.text)
+else:
+   print('Withdraw status: ' + str(w.status_code))
+   print("Account number: " + data['body']['acctNo'])
+   print("Success status: "+ str(data['body']['success']))
+   print("Withdraw amount: " +str(withdrawRequest['body']['amount']))
+   print("Balance: "+  str(data['body']['balance']- withdrawRequest['body']['amount']))
 
 print("=========Balance Request=========")
 
@@ -51,97 +56,29 @@ balanceRequest = {
    'head':{
       'fromCtry': 'MK',
       'fromBank': 'BANQ',
-      'toCtry': 'T1',
-      'toBank':'TEST'
+      'toCtry': 'MK',
+      'toBank':'BANQ'
    },
    'body':{
-      'acctNo':'MKBANQ0000000100',
-      'pin':'0000'
+      'acctNo':'MKBANQ0000000200',
+      'pin':'0001'
    }
 }
 bal_data = json.dumps(balanceRequest)
 
 b = requests.post('http://145.24.222.71:8443/balance',
-    cert = ('C:\school\Vakken\Project34-PinAutomaat\Keys\mk-server-chain.pem', 'C:\school\Vakken\Project34-PinAutomaat\Keys\country_key.pem'),
-    verify='C:\school\Vakken\Project34-PinAutomaat\Keys\mk-server-chain.pem',
-    headers=headers,
-    data=bal_data
+   cert = ('./keys/mk_server_chain.pem', './keys/country_key.pem'),
+   verify='./keys/noob_root.pem',
+   headers=headers,
+   data=bal_data
 )
-# print(bal_data)
-print(b.status_code)
-data_balance = json.loads(json.loads(b.text))
-print (data_balance)
-print('Balance status: ' + str(b.status_code))
-print("Account number: " + data_balance['body']['acctNo'])
-print("Balance: "+ str(data_balance['body']['amount']))
 
+try:
+   data_balance = json.loads(json.loads(b.text))
+except:
+   print(b.text)
+else:
+   print('Balance status: ' + str(b.status_code))
+   print("Account number: " + data_balance['body']['acctNo'])
+   print("Balance: "+ str(data_balance['body']['balance']))
 
-
-
-
-
-
-
-
-
-print("\nBalance status: " + str(b.status_code))
-# print(b.text)
-balance_response = b.json()
-print("fromCtry: " + balance_response["head"]["fromCtry"])
-print("fromBank: " + balance_response["head"]["fromBank"])
-print("toCtry: " + balance_response["head"]["toCtry"])
-print("toBank: " + balance_response["head"]["toBank"])
-print("Account number: " + balance_response["body"]["acctNo"])
-print("Success status: "+ str(balance_response["body"]["amount"]))
-
-headers = {"Content-Type": "application/json"}
-balancePayload = {
-   "head":{
-      "fromCtry":"MK",
-      "fromBank":"BANQ",
-      "toCtry":"T1",
-      "toBank":"TEST"
-   },
-   "body":{
-      "acctNo":"5",
-      "pin":"0101"
-   }
-}
-bal_data = json.dumps(balancePayload)
-print('Balance request: ' + bal_data+ '\n')
-
-b = requests.post('https://145.24.222.82:8443/api/balance',
-    cert = ('C:\school\Vakken\Project34-PinAutomaat\Keys\mk-server-chain.pem', 'C:\school\Vakken\Project34-PinAutomaat\Keys\country_key.pem'),
-    verify='C:/school/Vakken/Project34-PinAutomaat/Keys/145-24-222-82.pem',
-    headers=headers,
-    data=bal_data)
-print('Balance status: ' + str(b.status_code) + ',\n response: ' + b.text +'\n')
-
-withdrawPayload = {
-   "head":{
-      "fromCtry":"T1",
-      "fromBank":"TES",
-      "toCtry":"T1",
-      "toBank":"TES"
-   },
-   "body":{
-      "acctNo":"5",
-      "pin":"0101",
-      "amount": 5000
-   }
-}
-wdw_data = json.dumps(withdrawPayload)
-print('Withdraw request: ' + wdw_data+ '\n')
-
-
-w = requests.post('https://145.24.222.82:8443/api/withdraw',
-    cert = ('C:\school\Vakken\Project34-PinAutomaat\Keys\mk-server-chain.pem', 'C:\school\Vakken\Project34-PinAutomaat\Keys\country_key.pem'),
-    verify='C:\school\Vakken\Project34-PinAutomaat\Keys\mk-server-chain.pem',
-    headers=headers,
-    data=wdw_data)
-print('Withdraw status: ' + str(w.status_code) + ',\n response: ' + w.text+'\n')
-print("=============================================================")
-
-v = requests.get('https://145.24.222.82:8080', verify='C:\school\Vakken\Project34-PinAutomaat\Keys\mk-server-chain.pem')
-print(v.text)
-print(v.status_code)
